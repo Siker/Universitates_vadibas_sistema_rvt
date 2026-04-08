dfcc18539f15cb3244dd744e86ddbb5e3d076b15
24DP2IJudi <a240306ij@rvt.lv>
2026-04-08 14:33:01 +0300

    Update database connection parameters

--- db.py
+++ db.py
@@ -3,8 +3,8 @@ import mysql.connector
 # funkcija connect_db pieņem None tipa vērtību ievade un atgriež connection tipa vērtību savienojums
 def connect_db():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="пароль",
-        database="university_db"
+        host="sql.freedb.tech",
+        user="freedb_brat_andrej",
+        password="Uv5YN&e223Km4ay",
+        database="freedb_Universitates_vadibas_sistema"
     )
